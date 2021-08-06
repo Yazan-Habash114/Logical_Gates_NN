0d51515e3142ce23a2bddc5c5e17231920c691b9
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2021-08-06 20:01:20 +0300

    Testing

--- multilayer.py
+++ multilayer.py
@@ -5,58 +5,59 @@ In_data = [(0, 0), (0, 1), (1, 0), (1, 1)]
 out_data = [0, 1, 1, 0]
 delta_w_out = []
 delta_w_hidden = []
-delta_threshould = []
+delta_threshold = []
 input_layer = [Perceptron(), Perceptron()]
 
-for i in input_layer:
-    i.make_input()
+for input_perceptron in input_layer:
+    input_perceptron.make_input()
 
 hidden_layer = [Perceptron(), Perceptron()]
-for h in hidden_layer:
-    h.add_input()
-    h.weight[0], h.weight[1] = random.uniform(-1.2, 1.2), random.uniform(-1.2, 1.2)
-    print("weights are {0}".format(h.weight),h.threshold)
+for hidden in hidden_layer:
+    hidden.add_input()
+    hidden.weight[0], hidden.weight[1] = random.uniform(-1.2, 1.2), random.uniform(-1.2, 1.2)
+    print("weights are {0}".format(hidden.weight), hidden.threshold)
 
-outp = Perceptron()
-outp.add_input()
-outp.weight[0], outp.weight[1] = random.uniform(-1.2, 1.2), random.uniform(-1.2, 1.2)
-print("output layer are ", outp.weight, outp.threshold)
+out_perceptron = Perceptron()
+out_perceptron.add_input()
+out_perceptron.weight[0], out_perceptron.weight[1] = random.uniform(-1.2, 1.2), random.uniform(-1.2, 1.2)
+print("output layer are ", out_perceptron.weight, out_perceptron.threshold)
+
 ep = int(input("Enter # of epochs\n"))
 alpha = float(input("Enter learning rate\n"))
 
 for epoch in range(ep):
     print("Epoch # {0}".format(epoch + 1))
-    for i in range(len(In_data)):
+    for input_perceptron in range(len(In_data)):
         delta_w_out.clear()
         delta_w_hidden.clear()
-        delta_threshould.clear()
-        input_layer[0].In[0], input_layer[1].In[0] = In_data[i]
+        delta_threshold.clear()
+        input_layer[0].In[0], input_layer[1].In[0] = In_data[input_perceptron]
         for ip in input_layer:
             ip.get_output()
-        for h in hidden_layer:
-            h.In[0], h.In[1] = input_layer[0].out, input_layer[1].out
-            h.get_output()
-        outp.In[0], outp.In[1] = hidden_layer[0].out, hidden_layer[1].out
-        outp.get_output()
-        e = out_data[i] - outp.out
-        print("actual output is {0} desired output {1} error {2}".format(outp.out, out_data[i], e))
+        for hidden in hidden_layer:
+            hidden.In[0], hidden.In[1] = input_layer[0].out, input_layer[1].out
+            hidden.get_output()
+        out_perceptron.In[0], out_perceptron.In[1] = hidden_layer[0].out, hidden_layer[1].out
+        out_perceptron.get_output()
+        e = out_data[input_perceptron] - out_perceptron.out
+        print("actual output is {0} desired output {1} error {2}".format(out_perceptron.out, out_data[input_perceptron], e))
         #########################################################################
-        g5 = outp.out * (1 - outp.out) * e
-        for w in range(len(outp.weight)):
-            delta_w_out.append(alpha * outp.In[w] * g5)
-        delta_threshould.append(alpha * outp.wIn * g5)
-        for h in range(len(hidden_layer)):
-            gr = hidden_layer[h].out * (1 - hidden_layer[h].out) * (g5 * outp.weight[h])
-            for j in range(len(hidden_layer[h].weight)):
-                delta_w_hidden.append(alpha * hidden_layer[h].In[j] * gr)
-            delta_threshould.append(alpha * hidden_layer[h].wIn * gr)
-        for w in range(len(outp.weight)):
-            outp.weight[w] += delta_w_out[w]
+        g5 = out_perceptron.out * (1 - out_perceptron.out) * e
+        for w in range(len(out_perceptron.weight)):
+            delta_w_out.append(alpha * out_perceptron.In[w] * g5)
+        delta_threshold.append(alpha * out_perceptron.wIn * g5)
+        for hidden in range(len(hidden_layer)):
+            gr = hidden_layer[hidden].out * (1 - hidden_layer[hidden].out) * (g5 * out_perceptron.weight[hidden])
+            for j in range(len(hidden_layer[hidden].weight)):
+                delta_w_hidden.append(alpha * hidden_layer[hidden].In[j] * gr)
+            delta_threshold.append(alpha * hidden_layer[hidden].wIn * gr)
+        for w in range(len(out_perceptron.weight)):
+            out_perceptron.weight[w] += delta_w_out[w]
         index = 0
-        for h in hidden_layer:
-            for j in range(len(h.weight)):
-                h.weight[j] += delta_w_hidden[index]
+        for hidden in hidden_layer:
+            for j in range(len(hidden.weight)):
+                hidden.weight[j] += delta_w_hidden[index]
                 index += 1
-        outp.threshold += delta_threshould[0]
-        hidden_layer[0].threshold += delta_threshould[1]
-        hidden_layer[1].threshold += delta_threshould[2]
+        out_perceptron.threshold += delta_threshold[0]
+        hidden_layer[0].threshold += delta_threshold[1]
+        hidden_layer[1].threshold += delta_threshold[2]
